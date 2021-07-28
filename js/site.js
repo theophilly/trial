5f96dcd746000cff4063f5caa90fc0926c140d57
theodasa <theophilly20@gmail.com>
2021-07-28 22:49:52 +0100

    first commit

---
+++ js/site.js
@@ -0,0 +1,153 @@
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+const navLink = document.querySelectorAll('.nav_link');
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+navLink.forEach((n) => n.addEventListener('click', linkAction));
+const skillsContent = document.getElementsByClassName('skills_content'),
+  skillsHeader = document.querySelectorAll('.skills_header');
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills_content skills_close';
+  }
+  if (itemClass === 'skills_content skills_close') {
+    this.parentNode.className = 'skills_content skills_open';
+  }
+}
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+});
+const tabs = document.querySelectorAll('[data-target]'),
+  tabContents = document.querySelectorAll('[data-content]');
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('qualification_active');
+    });
+    target.classList.add('qualification_active');
+    tabs.forEach((tab) => {
+      tab.classList.remove('qualification_active');
+    });
+    tab.classList.add('qualification_active');
+  });
+});
+const modelViews = document.querySelectorAll('.services_model'),
+  modelBtns = document.querySelectorAll('.services_button'),
+  modelCloses = document.querySelectorAll('.services_model-close');
+let model = function (modelClick) {
+  modelViews[modelClick].classList.add('active-model');
+};
+modelBtns.forEach((modelBtn, i) => {
+  modelBtn.addEventListener('click', () => {
+    model(i);
+  });
+});
+modelCloses.forEach((modelClose) => {
+  modelClose.addEventListener('click', () => {
+    modelViews.forEach((modelView) => {
+      modelView.classList.remove('active-model');
+    });
+  });
+});
+let swiperPortfolio = new Swiper('.portfolio_container', {
+  cssMode: true,
+  loop: true,
+  navigation: { nextEl: '.swiper-button-next', prevEl: '.swiper-button-prev' },
+  pagination: { el: '.swiper-pagination', clickable: true },
+  mousewheel: true,
+  keyboard: true,
+});
+let swiperTestimonial = new Swiper('.testimonial_container', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: { 568: { slidesPerView: 2 } },
+});
+const sections = document.querySelectorAll('section[id]');
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute('id');
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.add('active-link');
+    } else {
+      document
+        .querySelector('.nav_menu a[href*=' + sectionId + ']')
+        .classList.remove('active-link');
+    }
+  });
+}
+window.addEventListener('scroll', scrollActive);
+function scrollHeader() {
+  const nav = document.getElementById('header');
+  if (this.scrollY >= 80) nav.classList.add('scroll-header');
+  else nav.classList.remove('scroll-header');
+}
+window.addEventListener('scroll', scrollHeader);
+function scrollUp() {
+  const scrollUp = document.getElementById('scroll-up');
+  if (this.scrollY >= 560) scrollUp.classList.add('show-scroll');
+  else scrollUp.classList.remove('show-scroll');
+}
+window.addEventListener('scroll', scrollUp);
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'uil-sun';
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+const getCurrentTheme = () =>
+  document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () =>
+  themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun';
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](
+    darkTheme
+  );
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](
+    iconTheme
+  );
+}
+themeButton.addEventListener('click', () => {
+  document.body.classList.toggle(darkTheme);
+  themeButton.classList.toggle(iconTheme);
+  localStorage.setItem('selected-theme', getCurrentTheme());
+  localStorage.setItem('selected-icon', getCurrentIcon());
+});
+var isNS = navigator.appName == 'Netscape' ? 1 : 0;
+if (navigator.appName == 'Netscape')
+  document.captureEvents(Event.MOUSEDOWN || Event.MOUSEUP);
+function mischandler() {
+  return false;
+}
+function mousehandler(e) {
+  var myevent = isNS ? e : event;
+  var eventbutton = isNS ? myevent.which : myevent.button;
+  if (eventbutton == 2 || eventbutton == 3) return false;
+}
+document.oncontextmenu = mischandler;
+document.onmousedown = mousehandler;
+document.onmouseup = mousehandler;
